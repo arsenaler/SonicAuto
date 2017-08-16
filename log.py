64f06b3565c1b7154e89a42d999d04ba760e24f8
arsenaler <youhaohaoxuexi@126.com>
2017-08-16 10:06:55 +0800

    update log.py file, add decorate function

--- log.py
+++ log.py
@@ -19,3 +19,22 @@ logger.addHandler(file_handle)
 logger.setLevel(logging.INFO)
 
 
+def use_logging(level):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if level == 'info':
+                logger.info('now we will run %s'%func.__name__)
+            elif level == 'debug':
+                logger.debug('now we will debug %s'%func.__name__)
+            elif level == 'warn':
+                logger.warn('now the %s has warn information'%func.__name__)
+                func(*args, **kwargs)
+            elif level == 'critical':
+                logger.critical('now the %s has critical information'%func.__name__)
+            elif level == 'error':
+                logger.error('now the %s has error information'%func.__name__)
+            else:
+                logger.exception('exception happen')
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
